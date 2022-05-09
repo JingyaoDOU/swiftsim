a153276c2bc8c243d67b18a6af0a07fcffd9f0dc
phirling <patrick.hirling@epfl.ch>
2022-05-09 21:46:32 +0200

    Applied Python formatting before merge

--- examples/GravityTests/Plummer_Selfgravitating/plummerIC.py
+++ examples/GravityTests/Plummer_Selfgravitating/plummerIC.py
@@ -24,33 +24,57 @@ import h5py
 import write_gadget as wg
 import argparse
 
-parser = argparse.ArgumentParser(description="Generate discrete realization of Anisotropic Plummer Model")
+parser = argparse.ArgumentParser(
+    description="Generate discrete realization of Anisotropic Plummer Model"
+)
 
 # Parse Main Arguments
-parser.add_argument("-a",type=float,default=0.05,help="Softening Length (in kpc)")
-parser.add_argument("-M",type=float,default=1.0e-5,help="Total Mass of Model (in 10^10 solar masses)")
-parser.add_argument("-q",type=float,default=1.0,help="Anisotropy parameter (between -inf and +2)")
-parser.add_argument("-N",type=int,default=1e4,help="Number of particles in the simulation")
-parser.add_argument("-bound",type=float,default=2.0,help="Upper bound of radius sampling")
-parser.add_argument("-boxsize",type=float,default=4.0,help="Box Size of simulation")
-parser.add_argument("--noparallel",action='store_false',help="Do not use multiprocessing library (default: false)")
-parser.add_argument("-nbthreads",type=int,default=6,help="Number of threads to use for multiprocessing (default: 6)")
+parser.add_argument("-a", type=float, default=0.05, help="Softening Length (in kpc)")
+parser.add_argument(
+    "-M", type=float, default=1.0e-5, help="Total Mass of Model (in 10^10 solar masses)"
+)
+parser.add_argument(
+    "-q", type=float, default=1.0, help="Anisotropy parameter (between -inf and +2)"
+)
+parser.add_argument(
+    "-N", type=int, default=1e4, help="Number of particles in the simulation"
+)
+parser.add_argument(
+    "-bound", type=float, default=2.0, help="Upper bound of radius sampling"
+)
+parser.add_argument("-boxsize", type=float, default=4.0, help="Box Size of simulation")
+parser.add_argument(
+    "--noparallel",
+    action="store_false",
+    help="Do not use multiprocessing library (default: false)",
+)
+parser.add_argument(
+    "-nbthreads",
+    type=int,
+    default=6,
+    help="Number of threads to use for multiprocessing (default: 6)",
+)
 
 args = parser.parse_args()
-if args.q > 2.0: raise ValueError("Anisotropy parameter must be smaller than 2.0 (q = {:.2f} given)".format(args.q))
+if args.q > 2.0:
+    raise ValueError(
+        "Anisotropy parameter must be smaller than 2.0 (q = {:.2f} given)".format(
+            args.q
+        )
+    )
 
 #### Parameters
 # Plummer Model
-G = 4.299581e+04       # Gravitational constant [kpc / 10^10 M_s * (kms)^2]
-a = args.a             # Plummer softening length [kpc]
-M = args.M             # Total Mass [10^10 M_s]
-q = args.q		       # Anisotropy Parameter (-inf,2]
+G = 4.299581e04  # Gravitational constant [kpc / 10^10 M_s * (kms)^2]
+a = args.a  # Plummer softening length [kpc]
+M = args.M  # Total Mass [10^10 M_s]
+q = args.q  # Anisotropy Parameter (-inf,2]
 
 # IC File
-N = int(args.N)        # Number of Particles
-bound = args.bound     # Max distance to origin (exclude outliers)
-fname = 'plummer.hdf5' # Name of the ic file (dont forget .hdf5)
-pickle_ics = 0         # Optional: Pickle ics (pos,vel,mass) for future use
+N = int(args.N)  # Number of Particles
+bound = args.bound  # Max distance to origin (exclude outliers)
+fname = "plummer.hdf5"  # Name of the ic file (dont forget .hdf5)
+pickle_ics = 0  # Optional: Pickle ics (pos,vel,mass) for future use
 box_size = args.boxsize
 periodic_bdry = 0
 
@@ -59,140 +83,185 @@ use_parallel = args.noparallel
 nb_threads = int(args.nbthreads)  # Set to None to use os.cpucount()
 
 ### Print parameters
-print('\n############################################################################################# \n')
-print('Generating Plummer ICs with the following parameters::\n')
-print('Softening length:                                  a = ' + "{:.3e} kpc".format(a))
-print('Total Mass:                                        M = ' + "{:.3e} Solar Masses".format(M*1e10))
-print('Anisotropy parameter:                              q = ' + str(q))
-print('Output file:                                       ' + fname)
-print('Number of particles:                               N = ' + "{:.1e}".format(N))
-print('\n############################################################################################# \n')
+print(
+    "\n############################################################################################# \n"
+)
+print("Generating Plummer ICs with the following parameters::\n")
+print(
+    "Softening length:                                  a = " + "{:.3e} kpc".format(a)
+)
+print(
+    "Total Mass:                                        M = "
+    + "{:.3e} Solar Masses".format(M * 1e10)
+)
+print("Anisotropy parameter:                              q = " + str(q))
+print("Output file:                                       " + fname)
+print("Number of particles:                               N = " + "{:.1e}".format(N))
+print(
+    "\n############################################################################################# \n"
+)
 
 # Estimate good softening length (density at origin, sphere s.t. contains on avg 1 particle)
-epsilon0 = a * N**(-1.0/3.0)
-print('Recommended Softening length (times conventional factor): ' + "{:.4e}".format(epsilon0) + ' kpc')
+epsilon0 = a * N ** (-1.0 / 3.0)
+print(
+    "Recommended Softening length (times conventional factor): "
+    + "{:.4e}".format(epsilon0)
+    + " kpc"
+)
 
 ### Generate Positions
 # Inverse transform sampling from analytical inverse of M(<r)
-def r_of_m(m,a,M):
-    return a * ((M/m)**(2./3.) - 1)**(-1./2.)
-m_rand = M*np.random.uniform(0.0,1.0,N)
-r_rand = r_of_m(m_rand,a,M)
-phi_rand = np.random.uniform(0.0,2*np.pi,N)
-theta_rand = np.arccos( np.random.uniform(-1.,1.,N) )
+def r_of_m(m, a, M):
+    return a * ((M / m) ** (2.0 / 3.0) - 1) ** (-1.0 / 2.0)
+
+
+m_rand = M * np.random.uniform(0.0, 1.0, N)
+r_rand = r_of_m(m_rand, a, M)
+phi_rand = np.random.uniform(0.0, 2 * np.pi, N)
+theta_rand = np.arccos(np.random.uniform(-1.0, 1.0, N))
 
 x = r_rand * np.sin(theta_rand) * np.cos(phi_rand)
 y = r_rand * np.sin(theta_rand) * np.sin(phi_rand)
 z = r_rand * np.cos(theta_rand)
 
-X = np.array([x,y,z]).transpose()
+X = np.array([x, y, z]).transpose()
 
 ### Generate Velocities
 # Sample the DF of the Plummer model at the radii generated above (give vr,vt)
 # Dejonghe (1987) Anisotropic Distribution Functionl
-normalization = 3.0*sci.gamma(6.0-q) / (2.0*(2.0*np.pi)**(2.5))
-units = G**(q-5.0) * M**(q-4.0) * a**(2.0-q)
+normalization = 3.0 * sci.gamma(6.0 - q) / (2.0 * (2.0 * np.pi) ** (2.5))
+units = G ** (q - 5.0) * M ** (q - 4.0) * a ** (2.0 - q)
 
-def H(E,L):
-    x = L**2 / (2.0*E*a**2)
-    if x<=1.0:
-        return 1.0/(sci.gamma(4.5-q)) * sci.hyp2f1(0.5*q,q-3.5,1.0,x)
+
+def H(E, L):
+    x = L ** 2 / (2.0 * E * a ** 2)
+    if x <= 1.0:
+        return 1.0 / (sci.gamma(4.5 - q)) * sci.hyp2f1(0.5 * q, q - 3.5, 1.0, x)
     else:
-        return 1.0/(sci.gamma(1.0-0.5*q)*sci.gamma(4.5-0.5*q)*(x**(0.5*q))) *sci.hyp2f1(0.5*q,0.5*q,4.5-0.5*q,1.0/x)
+        return (
+            1.0
+            / (sci.gamma(1.0 - 0.5 * q) * sci.gamma(4.5 - 0.5 * q) * (x ** (0.5 * q)))
+            * sci.hyp2f1(0.5 * q, 0.5 * q, 4.5 - 0.5 * q, 1.0 / x)
+        )
 
-def Fq(E,L):
+
+def Fq(E, L):
     if E < 0.0:
         return 0.0
     else:
-        return normalization * units * E**(3.5-q) * H(E,L) 
+        return normalization * units * E ** (3.5 - q) * H(E, L)
+
 
 # Helper Functions
 # Total energy: E = phi - 1/2v^2. relative potential: psi = -phi. Relative energy: -E = psi - 1/2v^2
-def relative_potential(r): # = - phi
-    return G*M / np.sqrt(r**2 + a**2)
+def relative_potential(r):  # = - phi
+    return G * M / np.sqrt(r ** 2 + a ** 2)
 
-def relative_energy(r,vr,vt):
-    return relative_potential(r) - 0.5 * (vr**2 + vt**2)
+
+def relative_energy(r, vr, vt):
+    return relative_potential(r) - 0.5 * (vr ** 2 + vt ** 2)
+
 
 # N.B: Angular momentum: L = r * vt
 
 # Convenience Function for scipy.minimize negative of Fq*vt, indep. vars passed as array
-def Fq_tomin(v,r):
-    return -Fq(relative_energy(r,v[0],v[1]),r*v[1])*v[1]
+def Fq_tomin(v, r):
+    return -Fq(relative_energy(r, v[0], v[1]), r * v[1]) * v[1]
+
 
 # Find max of DF at given radius
-def fmax(r,vmax):
+def fmax(r, vmax):
     # Constraint function (defined locally, dep on r)
     def vel_constr2(v):
-        return vmax**2-v[0]**2-v[1]**2
-    
+        return vmax ** 2 - v[0] ** 2 - v[1] ** 2
+
     # Initial Guess
-    v0 = [0.1*vmax,0.2*vmax]
+    v0 = [0.1 * vmax, 0.2 * vmax]
 
     # Constraint Object
-    cons = ({'type':'ineq', 'fun': vel_constr2})
-    
+    cons = {"type": "ineq", "fun": vel_constr2}
+
     # Args
     args = (r,)
 
     # Minimize through scipy.optimize.minimize
-    #fm = minimize(Fq_tomin,v0,constraints=cons,method = 'COBYLA',args=args)
-    fm = minimize(Fq_tomin,v0,constraints=cons,method = 'SLSQP',args=args,bounds=[(0,vmax),(0,vmax)])
-    
+    # fm = minimize(Fq_tomin,v0,constraints=cons,method = 'COBYLA',args=args)
+    fm = minimize(
+        Fq_tomin,
+        v0,
+        constraints=cons,
+        method="SLSQP",
+        args=args,
+        bounds=[(0, vmax), (0, vmax)],
+    )
+
     # Min of negative df == max of df
     return -fm.fun
+
 
 # Sample vr,vt from DF at given Radius through rejection sampling
 def sample_vel(r):
     # Compute max velocity (equiv. condition for E>=0)
-    vmax = np.sqrt(2. * relative_potential(r))
+    vmax = np.sqrt(2.0 * relative_potential(r))
     # Compute max of DF at this radius
-    fm = 1.1*fmax(r,vmax) # 1.1 factor to be sure to include max
+    fm = 1.1 * fmax(r, vmax)  # 1.1 factor to be sure to include max
     while True:
         # Select candidates for vr,vt based on max full velocity
         while True:
-            vr = np.random.uniform(0.0,vmax)
-            vt = np.random.uniform(0.0,vmax)
-            if (vr**2 + vt**2 <= vmax**2): break
+            vr = np.random.uniform(0.0, vmax)
+            vt = np.random.uniform(0.0, vmax)
+            if vr ** 2 + vt ** 2 <= vmax ** 2:
+                break
         # Rejection Sampling on Fq
-        f = np.random.uniform(0.0,fm)
-        if Fq(relative_energy(r,vr,vt),r*vt)*vt >= f:
-            return vr,vt
+        f = np.random.uniform(0.0, fm)
+        if Fq(relative_energy(r, vr, vt), r * vt) * vt >= f:
+            return vr, vt
 
-print('Sampling velocities...')
+
+print("Sampling velocities...")
 ti = time.time()
 if use_parallel:
-    from multiprocessing import Pool        
+    from multiprocessing import Pool
+
     with Pool(nb_threads) as p:
-        vels = np.array(p.map(sample_vel,r_rand)).transpose()
+        vels = np.array(p.map(sample_vel, r_rand)).transpose()
 else:
     from tqdm import tqdm
-    vels = np.empty((2,N))
-    for j,r in enumerate(tqdm(r_rand)):
-        vels[:,j] = sample_vel(r)
+
+    vels = np.empty((2, N))
+    for j, r in enumerate(tqdm(r_rand)):
+        vels[:, j] = sample_vel(r)
 tf = time.time()
-print('Sampling took ' + "{:.2f}".format(tf-ti) + ' seconds.')
+print("Sampling took " + "{:.2f}".format(tf - ti) + " seconds.")
 
 # Convert to Cartesian
 # First: project vt on e_theta, e_phi with random orientation
-alph = np.random.uniform(0,2*np.pi,N)
-sgn = np.random.choice([-1,1],size=N)
-vphi = vels[1]*np.cos(alph)
-vtheta = vels[1]*np.sin(alph)
+alph = np.random.uniform(0, 2 * np.pi, N)
+sgn = np.random.choice([-1, 1], size=N)
+vphi = vels[1] * np.cos(alph)
+vtheta = vels[1] * np.sin(alph)
 # project vr on e_r (random sign)
-vr = sgn*vels[0]
+vr = sgn * vels[0]
 
 # Convert Spherical to cartesian coordinates
-v_x = np.sin(theta_rand)*np.cos(phi_rand)*vr + np.cos(theta_rand)*np.cos(phi_rand)*vtheta - np.sin(phi_rand)*vphi
-v_y = np.sin(theta_rand)*np.sin(phi_rand)*vr + np.cos(theta_rand)*np.sin(phi_rand)*vtheta + np.cos(phi_rand)*vphi
-v_z = np.cos(theta_rand)*vr - np.sin(theta_rand)*vtheta
+v_x = (
+    np.sin(theta_rand) * np.cos(phi_rand) * vr
+    + np.cos(theta_rand) * np.cos(phi_rand) * vtheta
+    - np.sin(phi_rand) * vphi
+)
+v_y = (
+    np.sin(theta_rand) * np.sin(phi_rand) * vr
+    + np.cos(theta_rand) * np.sin(phi_rand) * vtheta
+    + np.cos(phi_rand) * vphi
+)
+v_z = np.cos(theta_rand) * vr - np.sin(theta_rand) * vtheta
 
 # Create velocity array
-V = np.array([v_x,v_y,v_z]).transpose()
+V = np.array([v_x, v_y, v_z]).transpose()
 
 ### Generate masses
 
-m = M/N * np.ones(N)
+m = M / N * np.ones(N)
 
 ### Exclude extreme outliers
 idx = r_rand < bound
@@ -202,46 +271,44 @@ m = m[idx]
 new_N = len(m)
 
 ### Write to hdf5
-print('Writing IC file...')
-with h5py.File(fname,'w') as f:
+print("Writing IC file...")
+with h5py.File(fname, "w") as f:
     wg.write_header(
         f,
         boxsize=box_size,
-        flag_entropy = 0,
-        np_total = [0,new_N,0,0,0,0],
-        np_total_hw = [0,0,0,0,0,0]
+        flag_entropy=0,
+        np_total=[0, new_N, 0, 0, 0, 0],
+        np_total_hw=[0, 0, 0, 0, 0, 0],
     )
-    wg.write_runtime_pars(
-        f,
-        periodic_boundary = periodic_bdry
-    )
+    wg.write_runtime_pars(f, periodic_boundary=periodic_bdry)
     wg.write_units(
         f,
-        length = 3.086e21,
-        mass = 1.988e43,
-        time = 3.086e21 / 1.0e5,
-        current = 1.0,
-        temperature = 1.0
+        length=3.086e21,
+        mass=1.988e43,
+        time=3.086e21 / 1.0e5,
+        current=1.0,
+        temperature=1.0,
     )
     wg.write_block(
         f,
-        part_type = 1,
-        pos = X,
-        vel = V,
-        mass = m,
+        part_type=1,
+        pos=X,
+        vel=V,
+        mass=m,
         ids=np.arange(new_N),
-        int_energy = np.zeros(new_N),
-        smoothing = np.ones(new_N)
+        int_energy=np.zeros(new_N),
+        smoothing=np.ones(new_N),
     )
 
 ### Optional: Pickle ic arrays for future use
 if pickle_ics:
     import pickle as pkl
-    with open('X.pkl','wb') as f:
-        pkl.dump(X[:],f)
 
-    with open('V.pkl','wb') as f:
-        pkl.dump(V[:],f)
+    with open("X.pkl", "wb") as f:
+        pkl.dump(X[:], f)
 
-    with open('M.pkl','wb') as f:
-        pkl.dump(m[:],f)
+    with open("V.pkl", "wb") as f:
+        pkl.dump(V[:], f)
+
+    with open("M.pkl", "wb") as f:
+        pkl.dump(m[:], f)
