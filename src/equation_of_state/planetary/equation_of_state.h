aa7c879800477f0d358ee702ff41cd627a02c54d
Jingyao Dou <qb20321@bristol.ac.uk>
2022-05-12 15:32:05 +0100

    Forked from planetary_gdf_etc. Test for the first time.

--- src/equation_of_state/planetary/equation_of_state.h
+++ src/equation_of_state/planetary/equation_of_state.h
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
+ * Edit by Jingyao on May 12th 2022
  ******************************************************************************/
 #ifndef SWIFT_PLANETARY_EQUATION_OF_STATE_H
 #define SWIFT_PLANETARY_EQUATION_OF_STATE_H
